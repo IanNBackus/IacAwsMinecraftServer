7d889277d248f4bc099878550ce19fb37de828d0
Klearo <Klearouwu@gmail.com>
2025-05-28 10:42:10 -0700

    EC2 provisioning scripts tested and working

---
+++ ProvisioningScripts/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "minecraft_security_group" {
+  name = "Project2MinecraftSecurityGroup"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "minecraft_elastic_ip" {
+  instance = aws_instance.minecraft_server.id
+}
+
+resource "aws_instance" "minecraft_server" {
+  ami                    = "ami-00565a15a71e4402a"
+  instance_type          = "t4g.small"
+  vpc_security_group_ids = [aws_security_group.minecraft_security_group.id]
+
+  tags = {
+    Name = "Project2MinecraftServer"
+  }
+}
